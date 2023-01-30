134e70b3f6faf6531266d4770af3e37d0da3260b
Aaron Dominguez <aronpes94@gmail.com>
2023-01-30 11:14:16 +0100

    DO-1383 Replace setup.py with setup.cfg

--- setup.py
+++ setup.py
@@ -1,12 +1,4 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-
-setup(
-    name='github-workflows-monitoring',
-    version='0.1',
-    long_description=__doc__,
-    packages=find_packages(),
-    include_package_data=True,
-    zip_safe=False,
-    install_requires=['Flask']
-)
+if __name__ == "__main__":
+    setup()
