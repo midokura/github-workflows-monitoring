90b0f12c6aba6ba42101f66554508a8620c98f38
Gerard Segarra <gerard.segarra@midokura.com>
2024-04-24 16:53:32 +0200

    Discard

--- src/app.py
+++ src/app.py
@@ -71,7 +71,7 @@ def process_workflow_job():
         node_ids.add(job.node_id)
 
     elif job.action == "in_progress":
-        node_ids.remove(job.node_id)
+        node_ids.discard(job.node_id)
         job_requested = jobs.get(job.id)
         time_to_start = None
         if not job_requested:
@@ -99,7 +99,7 @@ def process_workflow_job():
         jobs[job.id] = job
 
     elif job.action == "completed":
-        node_ids.remove(job.node_id)
+        node_ids.discard(job.node_id)
         job_requested = jobs.get(job.id)
         if not job_requested:
             app.logger.warning(f"Job {job.id} is {job.action} but not stored!")
