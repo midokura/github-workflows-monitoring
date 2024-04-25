f08329f48db1ce6abfd84ad4973f3db0a84d6a91
Gerard Segarra <gerard.segarra@midokura.com>
2024-04-25 11:22:15 +0200

    fixup! Make latest only on not PR

---
+++ src/job_processor.py
@@ -0,0 +1,20 @@
+def extract_jobs_metrics_from_data(jobs_data: dict, queued_node_ids_set: set):
+    jobs_metrics = []
+
+    for job in jobs_data["nodes"]:
+
+        if job["status"] != "queued":
+            queued_node_ids_set.discard(job["id"])
+            continue
+
+        context_details = {
+            "action": "monitor_queued",
+            "job_id": job["id"],
+            "job_name": job["name"],
+            "status": job["status"],
+            "started_at": job["startedAt"],
+            "completed_at": job["completedAt"],
+            }
+
+        jobs_metrics.append(context_details)
+    return jobs_metrics
