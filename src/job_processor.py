061631dcb511ba72603bad487ff1dc02b660d52f
Gerard Segarra <gerard.segarra@midokura.com>
2024-04-25 12:13:03 +0200

    Add metric of seconds_in_queue

--- src/job_processor.py
+++ src/job_processor.py
@@ -1,3 +1,6 @@
+from datetime import datetime
+
+
 def extract_jobs_metrics_from_data(jobs_data: dict, queued_node_ids_set: set):
     jobs_metrics = []
 
@@ -7,6 +10,9 @@ def extract_jobs_metrics_from_data(jobs_data: dict, queued_node_ids_set: set):
             queued_node_ids_set.discard(job["id"])
             continue
 
+        started_at = datetime.strptime(job["startedAt"], "%Y-%m-%dT%H:%M:%SZ")
+        now = datetime.now()
+
         context_details = {
             "action": "monitor_queued",
             "job_id": job["id"],
@@ -14,6 +20,7 @@ def extract_jobs_metrics_from_data(jobs_data: dict, queued_node_ids_set: set):
             "status": job["status"],
             "started_at": job["startedAt"],
             "completed_at": job["completedAt"],
+            "seconds_in_queue": (now - started_at).total_seconds(),
             }
 
         jobs_metrics.append(context_details)
