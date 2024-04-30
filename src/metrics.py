fa7c1576b12d868d5cc821ae8ceb8df785905370
Gerard Segarra <gerard.segarra@midokura.com>
2024-04-30 10:21:15 +0200

    debug

--- src/metrics.py
+++ src/metrics.py
@@ -1,5 +1,7 @@
 from datadog import initialize, statsd
 
+from flask import current_app
+
 options = {
     "statsd_host": "datadog-agent.datadog.svc.cluster.local",
     "statsd_port": 8125,
@@ -17,15 +19,19 @@ def send_queued_job(
     run_id: str,
     public: bool,
 ):
+    tags = [
+        f"job:{job_name}",
+        f"job_id:job-{job_id}",
+        f"repository:{repository}",
+        f"runner_name:{runner}",
+        f"run_id:run-{run_id}",  # "run-" added to group by run-id in DD
+        f"public:{public}",
+    ]
+    current_app.logger.info(
+        f"Submitting queue metric time: {seconds_in_queue}, tags: {tags}"
+    )
     statsd.histogram(
         "midokura.github_runners.jobs.seconds_in_queue.histogram",
         seconds_in_queue,
-        tags=[
-            f"job:{job_name}",
-            f"job_id:job-{job_id}",
-            f"repository:{repository}",
-            f"runner_name:{runner}",
-            f"run_id:run-{run_id}",  # "run-" added to group by run-id in DD
-            f"public:{public}",
-        ],
+        tags=tags,
     )
