91c75912fdb6e09fe40f47f75e0ff5c88c5d0184
Gerard Segarra <gerard.segarra@midokura.com>
2024-05-02 12:35:04 +0200

    Check with normalized tags

--- src/metrics.py
+++ src/metrics.py
@@ -1,4 +1,5 @@
 import logging
+import re
 
 from datadog import initialize, statsd
 from flask import current_app
@@ -13,6 +14,14 @@ initialize(**options)
 logger = logging.getLogger(__name__)
 
 
+TAG_INVALID_CHARS_RE = re.compile(r"[^\w\d_\-:/\.]", re.UNICODE)
+TAG_INVALID_CHARS_SUBS = "_"
+
+
+def normalize_tags(tag_list):
+    [TAG_INVALID_CHARS_RE.sub(TAG_INVALID_CHARS_SUBS, tag) for tag in tag_list]
+
+
 def send_queued_job(
     seconds_in_queue: int,
     job_name: str,
@@ -24,12 +33,14 @@ def send_queued_job(
 ):
     tags = [
         f"repository:{repository}",
-        # f"job:{job_name}",
+        f"job:{job_name}",
         f"status:{status}",
         # f"labels:{job_labels}",
         # f"public:{public}",
         # f"runner_group_name:{runner_group_name}",
     ]
+
+    tags = normalize_tags(tags)
 
     current_app.logger.info(f"Sending {seconds_in_queue} tags {tags}")
 
