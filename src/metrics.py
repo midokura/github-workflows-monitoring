0a355e3aaf9570b913717c3855b40233c7bca2dd
Gerard Segarra <gerard.segarra@midokura.com>
2024-05-02 12:18:12 +0200

    Tags

--- src/metrics.py
+++ src/metrics.py
@@ -1,5 +1,4 @@
 import logging
-import re
 
 from datadog import initialize, statsd
 from flask import current_app
@@ -23,21 +22,14 @@ def send_queued_job(
     public: bool,
     runner_group_name: str,
 ):
-    unprocessed_tags = [
+    tags = [
         f"repository:{repository}",
-        f"job:{job_name}",
+        # f"job:{job_name}",
         f"status:{status}",
-        # f"labels:{job_labels}",
+        f"labels:{job_labels}",
         # f"public:{public}",
         # f"runner_group_name:{runner_group_name}",
     ]
-    tags = []
-
-    for tag in unprocessed_tags:
-        splitted = tag.split(":")
-        subst = re.sub(r"\W+", "_", splitted[1])
-        tag = f"{splitted[0]}:{subst}"
-        tags.append(tag)
 
     current_app.logger.info(f"Sending {seconds_in_queue} tags {tags}")
 
